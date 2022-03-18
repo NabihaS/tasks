32364c772c0e92aa99bba62ee4b285e7b3243c6f
NabihaS <nazasy@udel.edu>
2022-03-18 00:07:46 -0400

    finished task 9

--- src/bad-components/ChooseTeam.tsx
+++ src/bad-components/ChooseTeam.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Button, Row, Col } from "react-bootstrap";
+import { StringDecoder } from "string_decoder";
 
 const PEOPLE = [
     "Alan Turing",
@@ -14,18 +15,16 @@ export function ChooseTeam(): JSX.Element {
     const [allOptions, setAllOptions] = useState<string[]>(PEOPLE);
     const [team, setTeam] = useState<string[]>([]);
 
-    function chooseMember() {
-        /*
+    function chooseMember(newMember: string) {
         if (!team.includes(newMember)) {
-            team.push(newMember);
+            const newteam = [...team, newMember];
+            setTeam(newteam);
         }
-        */
     }
 
     function clearTeam() {
-        /*
-        team = [];
-        */
+        const newteam: string[] = [];
+        setTeam(newteam);
     }
 
     return (
@@ -36,7 +35,10 @@ export function ChooseTeam(): JSX.Element {
                     {allOptions.map((option: string) => (
                         <div key={option} style={{ marginBottom: "4px" }}>
                             Add{" "}
-                            <Button onClick={chooseMember} size="sm">
+                            <Button
+                                onClick={() => chooseMember(option)}
+                                size="sm"
+                            >
                                 {option}
                             </Button>
                         </div>
