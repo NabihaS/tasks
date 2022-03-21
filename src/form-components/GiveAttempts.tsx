f1bdf7309da73ab85b73f644782c62233d452c3a
NabihaS <nazasy@udel.edu>
2022-03-21 10:08:53 -0400

    finished task 10

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,9 +1,38 @@
 import React, { useState } from "react";
-
+import { Form, Button } from "react-bootstrap";
+type ChangeEvent = React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>;
 export function GiveAttempts(): JSX.Element {
+    // the state (model)
+    const [attemptsLeft, setAttempts] = useState<number>(3);
+    const [requested, setRequested] = useState<number>(0);
+    //const requestedint = parseInt(requested) || "error";
+
+    // control
+    function setAttemptsLeft(): void {
+        setAttempts(attemptsLeft + requested);
+    }
+    function useAttempt(): void {
+        setAttempts(attemptsLeft - 1);
+    }
+    // view
     return (
         <div>
             <h3>Give Attempts</h3>
+            <span>Attempts: {attemptsLeft}</span>
+            <Form.Group controlId="formAttemptsRequested">
+                <Form.Label>Increase Attempts </Form.Label>
+                <Form.Control
+                    type="number"
+                    value={requested}
+                    onChange={(event: ChangeEvent) =>
+                        setRequested(parseInt(event.target.value) || 0)
+                    }
+                ></Form.Control>
+            </Form.Group>
+            <Button onClick={useAttempt} disabled={attemptsLeft === 0}>
+                Use
+            </Button>
+            <Button onClick={setAttemptsLeft}>Gain</Button>
         </div>
     );
 }
--- src/form-components/MultipleChoiceQuestion.tsx
+++ src/form-components/MultipleChoiceQuestion.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
-
+import { Form } from "react-bootstrap";
+type ChangeEvent = React.ChangeEvent<
+    HTMLTextAreaElement | HTMLInputElement | HTMLSelectElement
+>;
 export function MultipleChoiceQuestion({
     options,
     expectedAnswer
@@ -7,9 +10,28 @@ export function MultipleChoiceQuestion({
     options: string[];
     expectedAnswer: string;
 }): JSX.Element {
+    //the state
+    const [choice, setChoice] = useState<string>(options[0]);
+
+    //the controll
+    function updateChoice(event: ChangeEvent) {
+        setChoice(event.target.value);
+    }
+    // the view
     return (
         <div>
             <h3>Multiple Choice Question</h3>
+            <Form.Group controlId="userChoice">
+                <Form.Label>Whats the objectively best answer</Form.Label>
+                <Form.Select value={choice} onChange={updateChoice}>
+                    {options.map((choice: string) => (
+                        <option key={choice} value={choice}>
+                            {choice}
+                        </option>
+                    ))}
+                </Form.Select>
+            </Form.Group>
+            <div>{choice === expectedAnswer ? "✔️" : "❌"}</div>
         </div>
     );
 }
